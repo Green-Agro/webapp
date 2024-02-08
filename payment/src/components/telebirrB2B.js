c2916a89f8aba140547310d0bbe1734751b9cf85
Hana Habtemichael <hana.habtemichael@lersha.com>
2024-02-08 15:59:35 +0300

    axios method used

--- payment/src/components/telebirrB2B.js
+++ payment/src/components/telebirrB2B.js
@@ -1,48 +1,80 @@
 import  { useEffect,useState } from 'react';
+import axios from 'axios'
 import './loader.css';
+import { redirect } from 'react-router-dom';
 
 function TelebirrB2B() {
   const [error, setError] = useState(null);
   
+  console.log("Use effect is running")
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // Get the values from the URL query parameters
+  const amount = urlParams.get('amount');
+  const transactionId = urlParams.get('transaction_id');
+  const shortCode = urlParams.get('short_code');
+  const merchant_app_id = urlParams.get('merchant_app_id');
+  const [url,seturl]=useState('')
+  console.log(amount,"amount")
+  console.log(transactionId,"transactionId")
+  const formattedAmount = parseFloat(amount).toFixed(2).toString();
+
+  
+  const handleFetCH=()=>{
+     console.log("fetch")
+  try {
+    axios.post('http://196.189.118.76:3000/api/telebirr/pay',{
+      title: "Payment request",
+                amount: formattedAmount,
+                transaction_id: transactionId,
+                merchant_short_code: shortCode,
+                merchant_app_id: merchant_app_id,
+    })
+    .then(response => {
+      console.log("response",response.data)
+      window.open(response.data.url.trim(), "_self");
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setError(error.toString())
+    });
+    }catch(e){
+console.log("error ::", e);
+    }
+  }
+
+  
   useEffect(() => {
-      console.log("Use effect is running")
-      const urlParams = new URLSearchParams(window.location.search);
-
-      // Get the values from the URL query parameters
-      const amount = urlParams.get('amount');
-      const transactionId = urlParams.get('transaction_id');
-      const shortCode = urlParams.get('short_code');
-      const merchant_app_id = urlParams.get('merchant_app_id');
-      console.log(amount,"amount")
-      console.log(transactionId,"transactionId")
-
-      console.log(shortCode,"shortCode")
-
-      console.log(merchant_app_id,"merchant_app_id")
-
-      
-     
-      const formattedAmount = parseFloat(amount).toFixed(2).toString();
-
-      
-      fetch("http://196.189.118.76:3000/api/telebirr/pay", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title: "Payment request",
-          amount: formattedAmount,
-          transaction_id: transactionId,
-          short_code: shortCode,
-          merchant_app_id: merchant_app_id,
-        }),
-      })
-        .then((response) => {
+    handleFetCH()
+//       try {
+//       let response = fetch("", {
+//         method: "post",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           title: "Payment request",
+//           amount: formattedAmount,
+//           transaction_id: transactionId,
+//           merchant_short_code: shortCode,
+//           merchant_app_id: merchant_app_id,
           
-          console.log(response)
-          window.open(response.url.trim(), "_self");
-        })
-        .catch((err) => console.log("ERROR: ", err));
-        setError("An error occurred while processing the payment. Please try again later.");
+//         }),
+//       })
+//         .then((response) => {
+          
+//           console.log(response)
+//           setsuccess(true);
+//           console.log("success",success);
+//           //redirect(response.url.trim())
+//           //window.open(response.url.trim(), "_self");
+         
+          
+//         })
+//         .catch((err) => console.log("ERROR: ", err));
+//       }catch(e){
+// console.log("error ::", e);
+//       }
+
+      //  setError("An error occurred while processing the payment. Please try again later.");
   }, []);
 
   return (
